625fee7cca5d40b6106208b9e352d7f12bc5a641
Sunil Patil <sunilpatilsnd@gmail.com>
2024-12-18 17:29:23 +0530

    16x16 grid Version functional Done 1. 16x16 grid is created. 2. grid gets colored when mouse enters
    To Do/bugs
    1. old grid is not deleted when creating new one
    2. prompt entry value validation needs to be Done

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const body = document.querySelector('body');
+const width = window.innerWidth - 16; //deducting body margin value
+const height = window.innerHeight - 16; //deducting body margin value
 
-const container = document.createElement('div');
-container.classList = 'container';
+const body = document.querySelector('body'); //selecting body
 
-for(let i = 0; i < 256 ; i++ ){
-    let box = document.createElement('div');
-    box.classList = 'border';
-    box.textContent = i;
-    container.appendChild(box);
+const container = document.createElement('div'); //creating container
+container.classList = 'container' //adding class to container
+
+body.appendChild(container);  //adding container to body
+
+function createGrid(gridSize){
+    const container = document.querySelector('.container'); //selecting container
+     
+    let boxNum = gridSize**2; //calculating total num of boxes required  
+    for(let i = 0; i < boxNum ; i++ ){
+        let box = document.createElement('div'); //creating one box
+        box.classList = 'border box'; //adding border to box
+        let size = (width/gridSize) - 4; // adjusting for border size
+        box.style = `width: ${size}px; height: ${size}px`; //setting box size to be square
+        container.appendChild(box);                     //adding box to to container
+        box.addEventListener("mouseenter",()=>{     //registering event listener for each box and paint them black on mouse entry
+            box.classList.add('paint')
+        });
+        // box.textContent = i;
+    }
+    body.replaceChild(container, container);
 }
 
-body.appendChild(container);
+createGrid(16);
+
+const changeSize = document.querySelector('.gridder');
+changeSize.addEventListener("click", () =>{
+    let input = prompt('Enter size of the grid between 8-100');
+    let size = parseInt(input);
+    console.log(size);
+    createGrid(size);
+});
+
+
+
+
 
 
 
